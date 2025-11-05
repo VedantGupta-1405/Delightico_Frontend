f24457a74b91d49dc0aae6f7cb6daee64b7d2879
VedantGupta-1405 <vedantguptadsa@gmail.com>
2025-11-05 10:41:25 +0530

    Update ProductCard.jsx

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,12 +2,13 @@ import { Link } from "react-router-dom";
 
 export default function ProductCard({ product }) {
   return (
-    <div className="border rounded-lg shadow-sm p-4 hover:shadow-md transition">
+    // it creates a demo product name and creates the product image for the homepage view on the webpage |V|
+    <div className="border rounded-lg shadow-sm p-4 hover:shadow-md transition"> 
       <img src={product.image} alt={product.name} className="h-40 w-full object-cover" />
       <h3 className="mt-2 font-semibold">{product.name}</h3>
       <p className="text-gray-600">${product.price}</p>
       <Link
-        to={`/products/${product._id}`}
+        to={`/products/${product._id}`}c
         className="mt-2 inline-block bg-green-600 text-white px-4 py-1 rounded"
       >
         View
